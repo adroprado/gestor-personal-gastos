2809deb7f910165f48133f66af876d93a366a172
Alejandro Prado <adro.viprado@gmail.com>
2026-02-17 15:34:07 -0600

    feat(formulario): importar formulario y pasar las variables correspondientes

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 // --- Importación de Módulos ---
 import menuNavegacion from './js/menu-navegacion.js';
-
+import formularioGasto from './js/formulario.js';
 // --- Ejecución de Código ---
 document.addEventListener('DOMContentLoaded', (e) => {
   menuNavegacion('.btn-pestaña-gastos', '.btn-pestaña-estadisticas');
+  formularioGasto(
+    '.mensaje-exito',
+    '.lista-gastos',
+    '.informacion-monto',
+    '.informacion-registros',
+    '.plantilla',
+  );
 });
